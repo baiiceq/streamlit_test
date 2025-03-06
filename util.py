554b5de1c92bc29065abe7fcf62dbf57e46fccd8
wzyzsy <2579351233@qq.com>
2025-03-06 12:16:59 +0800

    update

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
 # MongoDB连接信息
-MONGO_URI='mongodb://localhost:27017/'
+MONGO_URI="mongodb+srv://testaccess:123321@wzy.idzma.mongodb.net/?retryWrites=true&w=majority&appName=wzy"
 
 # 数据库名称
-DB_NAME='dbtest'
+DB_NAME='db_test'
 
 # DeepSeek API密钥
 DEEPSEEK_API_KEY='sk-719621e0d17c4d39b0aa6a0ad3f21421'
